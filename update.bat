9865b705519a4c77ddb2d1f0e22097171646e2bd
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2019-07-17 15:28:55 -0400

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,1 @@
+choco upgrade all -y & pip-review --auto & Rscript -e "update.packages(type = 'binary', ask = FALSE, repos = 'https://cloud.r-project.org')" & UsoClient ScanInstallWait
---
+++ update.sh
@@ -0,0 +1,1 @@
+clear; printf "\n### TeX ###\n\n"; tlmgr update --self; tlmgr update --all; clear; printf "\n### Homebrew ###\n\n"; brew update; brew upgrade; brew cask upgrade; brew cleanup -s -prune=0; clear; printf "\n### Python ###\n\n"; pip-review --auto; clear; printf "\n### R ###\n\n"; Rscript <(echo $'update.packages(type = "binary", ask = FALSE, repos = "https://cloud.r-project.org")')
