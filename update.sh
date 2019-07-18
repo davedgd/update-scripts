cb9a7805cf50fee1278ae5a28f7ce7dec00d5e10
davedgd <4490587+davedgd@users.noreply.github.com>
2019-07-18 16:57:19 -0400

    Improve printouts on macOS

--- update.sh
+++ update.sh
@@ -1,30 +1,32 @@
-clear
+#!/bin/bash
 
-printf "\n### TeX ###\n\n"
+printf "\n"; clear
+
+printf "### TeX ###\n\n"
 tlmgr update --self
 tlmgr update --all
 
-clear
+printf "\n"; clear
 
-printf "\n### Homebrew ###\n\n"
+printf "### Homebrew ###\n\n"
 brew update
 brew upgrade
 brew cask upgrade
 brew cleanup -s -prune=0
 
-clear
+printf "\n"; clear
 
-printf "\n### Python ###\n\n"
+printf "### Python ###\n\n"
 pip-review --auto
 
-clear
+printf "\n"; clear
 
-printf "\n### R ###\n\n"
+printf "### R ###\n\n"
 Rscript <(echo $'update.packages(type = "binary", ask = FALSE, repos = "https://cloud.r-project.org")')
 
-clear
+printf "\n"; clear
 
-printf "\n### macOS ###\n\n"
+printf "### macOS ###\n\n"
 softwareupdate -i -a
 
-clear
+printf "\n"; clear
