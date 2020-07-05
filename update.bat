741af4ec3384b50ee684531bbe40f1219fb6448a
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2020-07-05 17:42:08 -0400

    swap pip with conda for Windows

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
 choco upgrade all -y
 powershell -command "Get-CimInstance -Namespace \"Root\cimv2\mdm\dmmap\" -ClassName \"MDM_EnterpriseModernAppManagement_AppManagement01\" | Invoke-CimMethod -MethodName UpdateScanMethod"
-pip-review --auto
+@rem pip-review --auto
+conda update --all -y
 Rscript -e "update.packages(type = 'binary', ask = FALSE, repos = 'https://cloud.r-project.org')"
 UsoClient ScanInstallWait
