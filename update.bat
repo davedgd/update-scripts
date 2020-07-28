cb900b880383e58a7e9202a35f3a392f6c14eb14
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2020-07-28 13:42:13 -0400

    add code to update pip packages outside of conda

--- update.bat
+++ update.bat
@@ -3,4 +3,19 @@ powershell -command "Get-CimInstance -Namespace \"Root\cimv2\mdm\dmmap\" -ClassN
 @rem pip-review --auto
 Rscript -e "update.packages(type = 'binary', ask = FALSE, repos = 'https://cloud.r-project.org')"
 UsoClient ScanInstallWait
+
+@echo off
+
+set packages=pip install --upgrade
+for /f "tokens=1" %%i in ('conda list ^| findstr /R /C:"pypi"') do (call :join %%i)
+@echo on
+%packages%
+@echo off
+goto :continue
+
+:join
+set packages=%packages% %1
+goto :eof
+
+:continue
 conda update --all -y
