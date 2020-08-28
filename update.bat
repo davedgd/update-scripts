05c155362f243599095c8ef02cd587da456cbaf1
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2020-08-28 11:02:54 -0400

    Update update.bat
    add MiKTeX updates for Windows

--- update.bat
+++ update.bat
@@ -1,4 +1,5 @@
 choco upgrade all -y
+mpm --admin --update
 powershell -command "Get-CimInstance -Namespace \"Root\cimv2\mdm\dmmap\" -ClassName \"MDM_EnterpriseModernAppManagement_AppManagement01\" | Invoke-CimMethod -MethodName UpdateScanMethod"
 @rem pip-review --auto
 Rscript -e "update.packages(type = 'binary', ask = FALSE, repos = 'https://cloud.r-project.org')"
