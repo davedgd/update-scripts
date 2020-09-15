88c9d7da6fe160ad0d365ab72001ea93183f6381
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2020-09-15 19:13:44 -0400

    fix cask updates on macOS and remove clear

--- update.sh
+++ update.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
-clear
-
 printf "\n### Homebrew ###\n\n"
 brew update
 brew upgrade
-brew cask upgrade
+brew upgrade --cask
 brew cleanup -s --prune='all'
-
-clear
 
 printf "\n### TeX ###\n\n"
 tlmgr update --self
 tlmgr update --all
 
-clear
-
 printf "\n### Python ###\n\n"
 #pip-review --auto
 conda update --all -y
 
-clear
-
 printf "\n### R ###\n\n"
 Rscript <(echo $'update.packages(type = "binary", ask = FALSE, repos = "https://cloud.r-project.org")')
 
-clear
-
 printf "\n### macOS ###\n\n"
 softwareupdate -i -a
-
-clear
