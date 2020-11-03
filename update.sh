c689920a310118d1ce52e423bf870126409f76ff
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2020-11-03 00:21:25 -0500

    add PyPI updates to macOS Python

--- update.sh
+++ update.sh
@@ -13,6 +13,7 @@ tlmgr update --all
 printf "\n### Python ###\n\n"
 #pip-review --auto
 conda update --all -y
+conda list | grep pypi | cut -d' ' -f1 | xargs pip install --upgrade # update pip packages
 
 printf "\n### R ###\n\n"
 Rscript <(echo $'update.packages(type = "binary", ask = FALSE, repos = "https://cloud.r-project.org")')
