521a316a5f327223ce53a6ffaf61cb889391751f
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2021-10-08 12:24:55 -0400

    back to conda (specifically, mamba)

--- update.bat
+++ update.bat
@@ -2,16 +2,16 @@ choco upgrade all -y
 call tlmgr update --self
 call tlmgr update --all
 powershell -command "Get-CimInstance -Namespace \"Root\cimv2\mdm\dmmap\" -ClassName \"MDM_EnterpriseModernAppManagement_AppManagement01\" | Invoke-CimMethod -MethodName UpdateScanMethod"
-pip-review --auto
+@rem pip-review --auto
 Rscript -e "update.packages(type = 'binary', ask = FALSE, repos = 'https://cloud.r-project.org')"
 UsoClient ScanInstallWait
 
-goto skip
+@rem goto skip
 
 @echo off
 
 set packages=pip install --upgrade
-for /f "tokens=1" %%i in ('conda list ^| findstr /R /C:"pypi"') do (call :join %%i)
+for /f "tokens=1" %%i in ('mamba list ^| findstr /R /C:"pypi"') do (call :join %%i)
 @echo on
 %packages%
 @echo off
@@ -22,7 +22,7 @@ set packages=%packages% %1
 goto :eof
 
 :continue
-call conda update --all -y
-call conda clean --all -y
+call mamba update --all -y
+call mamba clean --all -y
 
 :skip
