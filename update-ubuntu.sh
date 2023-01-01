8bd2612debbe8b5dbd277da8a491ab9ab7ddc18c
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2022-12-31 19:55:51 -0700

    update pip packages in addition to mamba

--- update-ubuntu.sh
+++ update-ubuntu.sh
@@ -8,6 +8,8 @@ sudo apt upgrade -y
 
 printf "\n### Python ###\n\n"
 mamba update --all
+mamba list | grep pypi | cut -d' ' -f1 | xargs pip install --upgrade # update pip packages
+mamba clean --all -y
 
 printf "\n### R ###\n\n"
 Rscript <(echo $'update.packages(type = "source", ask = FALSE, repos = "https://cloud.r-project.org")')
