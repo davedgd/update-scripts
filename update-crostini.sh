71160e4a33afd842f2b61bec2e8723b6d0fee94a
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2023-01-07 21:44:04 -0700

    Add crostini for ChromeOS Linux

---
+++ update-crostini.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+
+printf "\n### OS ###\n\n"
+sudo apt update
+sudo apt upgrade -y
+
+printf "\n### Flatpak ###\n\n"
+flatpak update
+
+printf "\n### Python ###\n\n"
+mamba update --all -y
+mamba list | grep pypi | cut -d' ' -f1 | xargs pip install --upgrade # update pip packages
+mamba clean --all -y
+
+printf "\n### R ###\n\n"
+Rscript <(echo $'update.packages(type = "source", ask = FALSE, repos = "https://cloud.r-project.org")')
