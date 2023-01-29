674ef000375ca1f215754234863bd9ffbe32ba85
David Dobolyi <4490587+davedgd@users.noreply.github.com>
2023-01-29 13:02:37 -0700

    add R (e.g., rpy2)

--- update-wsl.sh
+++ update-wsl.sh
@@ -11,3 +11,6 @@ printf "\n### Python ###\n\n"
 mamba update --all -y
 mamba list | grep pypi | cut -d' ' -f1 | xargs pip install --upgrade # update pip packages
 mamba clean --all -y
+
+printf "\n### R ###\n\n"
+Rscript <(echo $'update.packages(type = "source", ask = FALSE, repos = "https://cloud.r-project.org")')
